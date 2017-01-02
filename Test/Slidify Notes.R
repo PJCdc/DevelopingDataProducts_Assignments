26f5136dc08db94b03f8791f390ae74d1a861d27
PJCdc <MOOC@TMCmd.net>
2017-01-01 22:50:34 -0500

    Revert "Prelim Slidify Slides"
    This reverts commit 429b41d3cd10f97e83c8ef6ca7766ed01a2efac7.

--- Test/Slidify Notes.R
+++ Test/Slidify Notes.R
@@ -1,20 +1,5 @@
 
-# setwd("C:/Users/TCarroll/OneDrive - TMC/GitRepos/DevelopingDataProducts_Assignments")
-# http://slidify.org/start.html
+# setwd("C:/Users/TCarroll/OneDrive - TMC/GitRepos/DevelopingDataProducts_Assignments/Presentation")
 
 
 library(slidify)
-
-# Create a new slide deck: author("mydeck")
-author("Presentation")
-
-
-# Generate HTML silde deck
-# slidify("index.Rmd")
-
-# Publish to github
-# replace USER and REPO with your username and reponame
-# publish(user = "USER", repo = "REPO", host = 'github')
-
-# Publish to Tpubs
-# publish(title = 'mytitle', 'index.html', host = 'rpubs')
