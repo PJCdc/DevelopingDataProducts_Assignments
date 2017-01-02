429b41d3cd10f97e83c8ef6ca7766ed01a2efac7
PJCdc <MOOC@TMCmd.net>
2017-01-01 22:06:51 -0500

    Prelim Slidify Slides

--- Test/Slidify Notes.R
+++ Test/Slidify Notes.R
@@ -1,5 +1,20 @@
 
-# setwd("C:/Users/TCarroll/OneDrive - TMC/GitRepos/DevelopingDataProducts_Assignments/Presentation")
+# setwd("C:/Users/TCarroll/OneDrive - TMC/GitRepos/DevelopingDataProducts_Assignments")
+# http://slidify.org/start.html
 
 
 library(slidify)
+
+# Create a new slide deck: author("mydeck")
+author("Presentation")
+
+
+# Generate HTML silde deck
+# slidify("index.Rmd")
+
+# Publish to github
+# replace USER and REPO with your username and reponame
+# publish(user = "USER", repo = "REPO", host = 'github')
+
+# Publish to Tpubs
+# publish(title = 'mytitle', 'index.html', host = 'rpubs')
